1962778346eed7b4cac070b0efa08413319fde11
Mickael <mickael.spilmont@hospimedia-groupe.fr>
2019-12-20 12:23:03 +0100

    Adding an helper for display book list in index.html.erb

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,14 @@
 module BooksHelper
+  def book_link(book)
+    html = "<a href='/books/#{book.id}'>".html_safe
+    
+    if book.category
+      html += book.title + "(" + book.category.name + ")"
+    else
+      html += book.title
+    end
+
+    html += "</a>".html_safe
+    html
+  end
 end
